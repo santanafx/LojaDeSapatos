86f7af3642d016905a4958057bd645764b1dde8f
lucas <santanafx@hotmail.com>
2026-01-05 16:44:20 -0300

    feat: update Create method to return sapato ID and refactor GetById and GetAll methods

--- LojDeSapatos.Application/Service/Implementations/SapatoService.cs
+++ LojDeSapatos.Application/Service/Implementations/SapatoService.cs
@@ -1,7 +1,6 @@
-
-
 using LojDeSapatos.Application.InputModels;
 using LojDeSapatos.Application.Service.Interface;
+using LojDeSapatos.Application.ViewModels;
 using LojDeSapatos.Core.Entities;
 using LojDeSapatos.Infrastructure.Persistence;
 
@@ -15,13 +14,60 @@ namespace LojDeSapatos.Application.Service.Implementations
       _dbContext = dbContext;
     }
 
-    public string Create(NewSapatoInputModel inputModel)
+    public int Create(NewSapatoInputModel inputModel)
     {
       var id = System.Random.Shared.Next(1, int.MaxValue);
-      var sapato = new Sapato(id, inputModel.Tipo, inputModel.Preco, inputModel.Tamanho, inputModel.QuantidadeNoEstoque, inputModel.CreatedAt, inputModel.UpdatedAt);
+
+      var createdAt = inputModel.CreatedAt == default ? DateTime.Now : inputModel.CreatedAt;
+      var updatedAt = inputModel.UpdatedAt == default ? DateTime.Now : inputModel.UpdatedAt;
+
+      var sapato = new Sapato(
+        id,
+        inputModel.Tipo,
+        inputModel.Preco,
+        inputModel.Tamanho,
+        inputModel.QuantidadeNoEstoque,
+        createdAt,
+        updatedAt
+      );
       _dbContext.Sapatos.Add(sapato);
 
-      return "Sapato criado com sucesso!";
+      return id;
+    }
+
+    public SapatoViewModel? GetById(int id)
+    {
+      var sapato = _dbContext.Sapatos.SingleOrDefault(p => p.Id == id);
+
+      if (sapato == null)
+      {
+        return null;
+      }
+
+      var sapatoViewModel = new SapatoViewModel(
+        sapato.Id,
+        sapato.Tipo,
+        sapato.Preco,
+        sapato.Tamanho,
+        sapato.QuantidadeNoEstoque
+      );
+
+      return sapatoViewModel;
+    }
+
+    public List<SapatoViewModel> GetAll()
+    {
+      var sapatos = _dbContext.Sapatos
+        .Select(s => new SapatoViewModel(
+          s.Id,
+          s.Tipo,
+          s.Preco,
+          s.Tamanho,
+          s.QuantidadeNoEstoque
+        ))
+        .ToList();
+
+      return sapatos;
     }
   }
 }
