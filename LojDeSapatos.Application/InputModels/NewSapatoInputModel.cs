fb5711db148deccc44e3d40489ccba3d71bdefbd
lucas <santanafx@hotmail.com>
2026-01-05 16:44:09 -0300

    feat: update NewSapatoInputModel properties to allow public setters for Tipo, Preco, Tamanho, and QuantidadeNoEstoque

--- LojDeSapatos.Application/InputModels/NewSapatoInputModel.cs
+++ LojDeSapatos.Application/InputModels/NewSapatoInputModel.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LojDeSapatos.Application.InputModels
 {
   public class NewSapatoInputModel
   {
     public int Id { get; private set; }
-    public string Tipo { get; private set; }
-    public decimal Preco { get; private set; }
-    public int Tamanho { get; private set; }
-    public int QuantidadeNoEstoque { get; private set; }
+
+    [Required]
+    public string Tipo { get; set; } = string.Empty;
+
+    public decimal Preco { get; set; }
+    public int Tamanho { get; set; }
+    public int QuantidadeNoEstoque { get; set; }
+
     public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
   }
